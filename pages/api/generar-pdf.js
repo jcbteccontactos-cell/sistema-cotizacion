242e4f3f3da1f588b96ceedcd175a1a22e61f91c
jcbtecnology <jcbteccontactos@gmail.com>
2026-03-14 08:18:41 -0500

    fix generar pdf vercel

--- pages/api/generar-pdf.js
+++ pages/api/generar-pdf.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath: await chromium.executablePath(),
-      headless: chromium.headless,
+      headless: true,
     });
 
     const page = await browser.newPage();
